e7c35a354a9a8e68c2a232bebf49cb421d741cb4
elambrechtsen <eloise.lambrechtsen@marsden.school.nz>
2023-05-09 11:00:32 +1200

    starting form

--- server.py
+++ server.py
@@ -8,18 +8,23 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/Meet the team")
-def menu():
+@app.route("/meet_the_team")
+def meet_the_team():
     return render_template("meet_the_team.html")
 
 
-@app.route("/Upcoming events")
+@app.route("/upcoming_events")
 def upcoming_events():
     return render_template("upcoming_events.html")
 
-@app.route("/Get involved")
+
+@app.route("/get_involved", methods=["GET", "POST"])
 def get_involved():
     return render_template("get_involved.html")
+
+@app.route("/news")
+def news():
+    return render_template("news.html")
 
 
 if __name__ == "__main__":
